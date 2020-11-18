55e2d42d2315d6a0a22c811cbd421ce81e1aa5c7
comingforyou20 <57737427+comingforyou20@users.noreply.github.com>
2020-11-18 15:57:52 +0700

    final1

---
+++ j/menu.js
@@ -0,0 +1,145 @@
+
+var myvar = '<div class="menubg" id="menubg" onclick="closemenu()">'+
+'		<div class="menu">'+
+'			<div class="title"><a href="index.html">Trang chủ</a></div>'+
+''+
+'			<div class="title"><a href="kichban.html">Kịch bản</a></div>'+
+'			<div class="lanmu">'+
+'				<ul>'+
+'					<li><a href="kichban.html#kb1">Kịch bản 1</a> </li>'+
+'					<li><a href="kichban.html#kb2">Kịch bản 2</a> </li>'+
+'					<li><a href="kichban.html#kb3">Kịch bản 3</a> </li>'+
+'					<li><a href="kichban.html#kb4">Kịch bản 4</a> </li>'+
+'					<li><a href="kichban.html#kb5">Kịch bản 5</a> </li>'+
+'				</ul>'+
+'			</div>'+
+''+
+'			<div class="title"><a href="tamthoai.html">Đại Pháp hồng truyền</a></div>'+
+'			<div class="lanmu">'+
+'				<ul>'+
+'					<li><a href="dpht.html#dp1">Kịch bản giảng Đại Pháp hồng truyền</a> </li>'+
+'					<li><a href="dpht.html#dp2">Chân Thiện Nhẫn là gì?</a> </li>'+
+'					<li><a href="dpht.html#dp3">Phân biệt PLC và Phật giáo</a> </li>'+
+'					<li><a href="dpht.html#dp4">Pháp Luân Công có lợi ích gì?</a> </li>'+
+'					<li><a href="dpht.html#dp5">Chúng sinh hiểu lầm về Sư phụ</a> </li>'+
+'					<li><a href="dpht.html#dp6">ShenYun Văn hóa truyền thống</a> </li>'+
+'					<li><a href="dpht.html#dp17">Tham khảo thêm</a> </li>'+
+'				</ul>'+
+'			</div>'+
+''+
+''+
+'			<div class="title"><a href="tuthieu.html">Tự thiêu</a></div>'+
+'			<div class="lanmu">'+
+'				<ul>'+
+'					<li><a href="tuthieu.html#tt1">Kịch bản GCT tự thiêu</a> </li>'+
+'					<li><a href="tuthieu.html#tt2">Giảng ngắn - Tự thiêu</a> </li>'+
+'					<li><a href="tuthieu.html#tt3">Tham khảo thêm</a> </li>'+
+'				</ul>'+
+'			</div>'+
+'			<div class="title"><a href="254.html">Sự kiện 25/4</a></div>'+
+'			<div class="lanmu">'+
+'				<ul>'+
+'					<li><a href="254.html#2541">Kịch bản GCT 25/4</a> </li>'+
+'					<li><a href="254.html#2542">Không đến Trung Nam Hải thì không có bức hại?</a> </li>'+
+'					<li><a href="254.html#2543">Thỉnh nguyện 25/4 đã dẫn đến bức hại?</a> </li>'+
+'					<li><a href="254.html#2544">Nhiều người tham gia thỉnh nguyện mới dẫn đến đàn áp?</a> </li>'+
+'					<li><a href="254.html#2545">Tín ngưỡng được nhưng làm chính trị không được?</a> </li>'+
+''+
+'				</ul>'+
+'			</div>'+
+'			<div class="title"><a href="1400.html">1400 trường hợp</a></div>'+
+'			<div class="lanmu">'+
+'				<ul>'+
+'					<li><a href="mocuop.html#14001">Kịch bản GCT 1400</a> </li>'+
+'					<li><a href="mocuop.html#14002">Mổ bụng tìm Pháp Luân ?</a> </li>'+
+'					<li><a href="mocuop.html#14003">Tham khảo thêm</a> </li>'+
+'				</ul>'+
+'			</div>'+
+''+
+''+
+'			<div class="title"><a href="mocuop.html">Mổ cướp tạng</a></div>'+
+'			<div class="lanmu">'+
+'				<ul>'+
+'					<li><a href="mocuop.html#mc1">Kịch bản GCT mổ cướp tạng</a> </li>'+
+'					<li><a href="mocuop.html#mc2">Tham khảo thêm</a> </li>'+
+'				</ul>'+
+'			</div>'+
+''+
+'			<div class="title"><a href="tien.html">Xin tiền</a></div>'+
+'			<div class="lanmu">'+
+'				<ul>'+
+'					<li><a href="tien.html#ct1">Cho tiền tôi sẽ thoái 1</a> </li>'+
+'					<li><a href="tien.html#ct2">Cho tiền tôi sẽ thoái 2</a> </li>'+
+'					<li><a href="tien.html#ct3">Cho tiền tôi sẽ thoái 3</a> </li>'+
+'					<li><a href="tien.html#ct4">Tham gia Pháp Luân Công có được tiền không?</a> </li>'+
+'					<li><a href="tien.html#ct5">Tham gia Pháp Luân Công có được tiền không 2?</a> </li>'+
+'					<li><a href="tien.html#ct6">Tham khảo thêm</a> </li>'+
+'				</ul>'+
+'			</div>'+
+''+
+'			<div class="title"><a href="vothan.html">Vô thần</a></div>'+
+'			<div class="lanmu">'+
+'				<ul>'+
+'					<li><a href="vothan.html#vt1">Vô thần chỉ tin khoa học</a> </li>'+
+'					<li><a href="vothan.html#vt2">Thần ở đâu? Tôi không thấy ?</a> </li>'+
+'				</ul>'+
+'			</div>'+
+''+
+'			<div class="title"><a href="songtot.html">Đang sống tốt</a></div>'+
+'			<div class="lanmu">'+
+'				<ul>'+
+'					<li><a href="songtot.html#st1">Tôi đang sống tốt 1</a> </li>'+
+'					<li><a href="songtot.html#st2">Tôi đang sống tốt 2</a> </li>'+
+'					<li><a href="songtot.html#st3">Tôi đang sống tốt 3</a> </li>'+
+'					<li><a href="songtot.html#st4">Tham khảo thêm</a> </li>'+
+'				</ul>'+
+'			</div>'+
+''+
+'			<div class="title"><a href="codoc.html">GCT cho Cơ đốc giáo</a></div>'+
+'			<div class="lanmu">'+
+'				<ul>'+
+'					<li><a href="codoc.html#cd1">GCT cho người Cơ đốc giáo</a> </li>'+
+'					<li><a href="codoc.html#cd2">Khuyên thoái</a> </li>'+
+'					<li><a href="codoc.html#cd3">Tham khảo thêm</a> </li>'+
+'				</ul>'+
+'			</div>'+
+''+
+'			<div class="title"><a href="antoan.html">An toàn telegram</a></div>'+
+''+
+'			<div class="title"><a href="giaotiep.html">Giao tiếp</a></div>'+
+'			<div class="lanmu">'+
+'				<ul>'+
+'						<li><a href="giaotiep.html#gt1">Chào hỏi</a> </li>'+
+'					<li><a href="giaotiep.html#gt2">Tạm biệt</a> </li>'+
+'					<li><a href="giaotiep.html#gt3">Xin lỗi</a> </li>'+
+'					<li><a href="giaotiep.html#gt4">Chúc mừng</a> </li>'+
+'					<li><a href="giaotiep.html#gt5">Tham khảo thêm</a> </li>'+
+'				</ul>'+
+'			</div>'+
+'			<div class="title"><a href="khac.html">Trường hợp khác</a></div>'+
+'			<div class="lanmu">'+
+'				<ul>'+
+'					<li><a href="khac.html#k1">ĐCSTQ thối nát. Tôi cũng bất lực</a> </li>'+
+'					<li><a href="khac.html#k2">Đây toàn là giả ?</a> </li>'+
+'					<li><a href="khac.html#k3">Vượt tường lửa</a> </li>'+
+'					<li><a href="khac.html#k4">Bệnh dịch Mỹ cũng chết nhiều</a> </li>'+
+'					<li><a href="khac.html#k5">Tại sao cứ bảo DCSTQ ác ?</a> </li>'+
+'					<li><a href="khac.html#k6">Giang Trạch Dân chưa bị quả báo?</a> </li>'+
+'					<li><a href="khac.html#k7">Có được vợ không? (hoặc họ gửi clip sắc dục)</a> </li>'+
+'					<li><a href="khac.html#k8">Thoái rồi mà ko có phúc báo ?</a> </li>'+
+'					<li><a href="khac.html#k9">Làm chính trị</a> </li>'+
+'					<li><a href="khac.html#k10">Phản đảng là phản động?</a> </li>'+
+'				</ul>'+
+'			</div>'+
+''+
+''+
+'		</div>	'+
+'	</div>'+
+''+
+''+
+'	<div class="menubg" id="menubg2" onclick="closemenu()">'+
+'		<div class="menu menu2">'+
+'		</div>	'+
+'	</div>';
+	
+document.write(myvar);
