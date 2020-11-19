6f11e332a71056f87b319a670b3e3660af91bc2a
comingforyou20 <57737427+comingforyou20@users.noreply.github.com>
2020-11-19 09:44:32 +0700

    fix menu

--- j/menuhead.js
+++ j/menuhead.js
@@ -1,6 +1,6 @@
 
 var myvar = '<div id="head">'+
-'	<h1>Crimeofccp V2</h1>'+
+'	<h1><a href="/search/">Crimeofccp V2</a></h1></h1>'+
 '	<div id="headm">'+
 '	<h2 class="i1"><a target="blank" href="/searchv1/">Version 1</a></h2></h2>'+
 '	<h2 class="i2" onclick="openmenu2()">Menu 2</h2>'+
